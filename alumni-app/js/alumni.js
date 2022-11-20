c9f40b26bbe7d924720483e58a55c97611ed9483
Sanjero <gelosantos72@gmail.com>
2022-11-20 22:41:39 +0800

    Add clone function

--- alumni-app/js/alumni.js
+++ alumni-app/js/alumni.js
@@ -1,3 +1,26 @@
-import { token_check } from "./modules/constant-token-checker.js";
+import { token_check } from './modules/constant-token-checker.js';
 
-token_check('#', '/index.html');
+// token_check('#', '/index.html');
+
+// dummy data
+
+const CONTENT_CONTAINER = document.querySelector('.content-container');
+
+let temp = document.getElementById('alumni-template');
+
+function createClone() {
+  const clone = document.importNode(temp.content, true);
+  CONTENT_CONTAINER.appendChild(clone);
+
+  const infos = clone.querySelectorAll('.infos > li');
+  infos.forEach(info => console.log(info.textContent));
+}
+
+createClone();
+createClone();
+createClone();
+createClone();
+createClone();
+createClone();
+createClone();
+createClone();
