0cee29fc57632883ca130e6eafd9bba75d9bdd88
Sanjero <gelosantos72@gmail.com>
2022-12-02 03:04:40 +0800

    Allow edit on textareas

--- alumni-app/js/profile.js
+++ alumni-app/js/profile.js
@@ -1,8 +1,12 @@
 import { token_check } from './modules/constant-token-checker.js';
-import { underConstruction } from './modules/alert.js';
 
 token_check('#', '/index.html');
 
-const main = document.querySelector('main');
-// underConstruction(main);
+const editBtn = document.querySelector('.edit-btn');
+const textBoxes = document.querySelectorAll('textarea');
 
+editBtn.addEventListener('click', () => {
+  textBoxes.forEach((box) => {
+    box.classList.toggle('editable');
+  });
+});
