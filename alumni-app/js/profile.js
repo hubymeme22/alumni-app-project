4c5de21236a8868dac821163fbdfe5acf5a5d623
Sanjero <gelosantos72@gmail.com>
2022-12-03 04:42:29 +0800

    generate href based on links

--- alumni-app/js/profile.js
+++ alumni-app/js/profile.js
@@ -2,30 +2,13 @@ import { token_check } from './modules/constant-token-checker.js';
 import { checkGender } from './modules/check-gender.js';
 
 token_check('#', '/index.html');
+
 const btnContainer = document.querySelector('.btns');
 
 const editBtn = document.getElementById('edit-btn');
 const cancelBtn = document.getElementById('cancel-btn');
 const saveBtn = document.getElementById('save-btn');
 const textBoxes = document.querySelectorAll('textarea');
-
-editBtn.addEventListener('click', () => {
-  btnContainer.classList.remove('hide');
-  editBtn.classList.add('hide');
-
-  textBoxes.forEach((box) => {
-    box.classList.add('editable');
-  });
-});
-
-saveBtn.addEventListener('click', () => {
-  btnContainer.classList.add('hide');
-  editBtn.classList.remove('hide');
-
-  textBoxes.forEach((box) => {
-    box.classList.remove('editable');
-  });
-});
 
 // DOM Elements
 const profileImg = document.getElementById('profile-img');
@@ -43,28 +26,96 @@ const twitterLink = document.getElementById('twitter-link');
 const linkedInLink = document.getElementById('linkedin-link');
 const githubLink = document.getElementById('github-link');
 
+const allLinks = [facebookLink, twitterLink, linkedInLink, githubLink];
+const allTextBoxes = [facebook, twitter, linkedIn, github];
+
+// Event Listeners
+editBtn.addEventListener('click', () => {
+  btnContainer.classList.remove('hide');
+  editBtn.classList.add('hide');
+
+  textBoxes.forEach((box) => {
+    box.classList.add('editable');
+  });
+
+  editData();
+});
+
+saveBtn.addEventListener('click', () => {
+  btnContainer.classList.add('hide');
+  editBtn.classList.remove('hide');
+
+  textBoxes.forEach((box) => {
+    box.classList.remove('editable');
+  });
+
+  saveData();
+});
+
+cancelBtn.addEventListener('click', () => {
+  btnContainer.classList.add('hide');
+  editBtn.classList.remove('hide');
+
+  textBoxes.forEach((box) => {
+    box.classList.remove('editable');
+  });
+});
+
 function generateInfo(user) {
   // Get data from database and adjust DOM elements contents
   profileImg.src = checkGender(user.sex);
-
-  username.textContent = `@${user.firstName} ${user.lastName}`;
-  // facebook.textContent =
+  username.textContent = `${user.username}`;
 }
 
 const sampleData = {
   username: 'genderbender',
-  email: 'sample@gmail.com',
-  firstName: 'Paul',
-  lastName: 'Enriquez',
-  sex: 'male',
+  sex: 'Male',
+  links: {
+    facebook: '#',
+    twitter: '#',
+    linkedin: '#',
+    github: '#',
+  },
 };
 
-// facebook.addEventListener('click', () => {
-//   if (facebook.value.trim() != '') {
-//     facebookLink.href = facebook.value;
-//     facebookLink.textContent = facebook.value;
-//     facebook.classList.add('hide');
-//   } else {
-//     facebook.classList.remove('hide');
-//   }
-// });
+generateInfo(sampleData);
+
+function collectData() {
+  return {
+    username: username.value,
+    sex: 'Male',
+    links: [facebook.value, twitter.value, linkedIn.value, github.value],
+  };
+}
+
+function saveData() {
+  const data = collectData();
+
+  console.log(allTextBoxes[1]);
+  for (let i = 0; i < 4; i++) {
+    if (data.links[i] != '' || data.links[i] != '#') {
+      allTextBoxes[i].classList.add('hide');
+      allLinks[i].classList.remove('hide');
+      allLinks[i].href = `https://${data.links[i]}`;
+      allLinks[i].textContent = data.links[i];
+    }
+
+    if (data.links[i] == '') {
+      allTextBoxes[i].classList.remove('hide');
+    }
+  }
+}
+
+function editData() {
+  showTextBoxes();
+}
+
+function showTextBoxes() {
+  allLinks.forEach((link) => {
+    link.classList.add('hide');
+  });
+
+  allTextBoxes.forEach((box) => {
+    box.classList.remove('hide');
+  });
+}
