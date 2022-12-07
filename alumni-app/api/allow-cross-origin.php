72c78ea4b58ac92be1e7a0e1b7f4a1b6a3e43e13
HueHueberry <huby.new1112@gmail.com>
2022-12-07 15:22:40 +0800

    Allow credentials api

--- alumni-app/api/allow-cross-origin.php
+++ alumni-app/api/allow-cross-origin.php
@@ -1,6 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://192.168.1.10:8100");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Credentials: true");
 
 ?>
